5ae1b82fb721faa3558ccc502672fe4523649add
Khushi Agarwal <kagarwal1676@gmail.com>
2024-04-20 01:09:05 +0530

    Landing Page Done

--- krishikaya/src/App.js
+++ krishikaya/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import header from "./assets/Project.jpg";
 import intro from "./assets/farm.png";
-import footer from "./assets/logo.png"
+import footer from "./assets/logo.png";
 function App() {
   return (
     <div className="App">
@@ -14,38 +14,86 @@ function App() {
       </header>
       <div className="main-body">
         <div className="intro">
-             <div className="img_intro">
-                 <img src={intro} alt="about"></img>
-             </div>
-             <div className="intro_cont">
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci magnam consectetur ipsam, ab aut optio inventore quam, quia porro voluptatum maiores suscipit, officia cumque totam et reprehenderit consequuntur tempore ipsum.
-             </div>
+          <div className="img_intro">
+            <img src={intro} alt="about"></img>
+          </div>
+          <div className="intro_cont">
+            A comprehensive farmer assist app to streamline agricultural
+            operations by predicting weather conditions, facilitating inventory
+            management, providing access to a marketplace for buying/selling
+            produce, and delivering updates on news and government schemes
+            relevant to farmers.
+            <br></br>
+            Our vision is to revolutionize the agricultural landscape by
+            bridging the gap between traditional farming practices and modern
+            technological advancements.
+            <br></br>
+          </div>
         </div>
         <div className="movingtext">
           <div className="write">
-            <span>Feature 1 Feature 2 </span>
+            <span>
+              Inventory Management -- Weather Forecasting -- Market Place -- AI
+              ChatBot --{" "}
+            </span>
           </div>
           <div className="write marquee2">
-            <span>Feature 1 Feature 2 </span>
+            <span>
+              Inventory Management -- Weather Forecasting -- Market Place -- AI
+              ChatBot --{" "}
+            </span>
           </div>
         </div>
         <div className="features">
           <div className="f">
-             <div className="f_img1"></div>
+            <div className="co f_img1"></div>
+            <div className="con">
+              <h3>Inventory Management</h3>
+              <p>
+                Efficiently track, manage, and optimize farm inputs and outputs.
+              </p>
+            </div>
           </div>
           <div className="f fe">
-          <div className="f_img2"></div>
+            <div className="co f_img2"></div>
+            <div className="con">
+              <h3>Market Place</h3>
+              <p>
+                Connect farmers directly with buyers and sellers, facilitating
+                transparent transactions.
+              </p>
+            </div>
           </div>
           <div className="f">
-          <div className="f_img3"></div>
+            <div className="co f_img3"></div>
+            <div className="con">
+              <h3>AI ChatBot</h3>
+              <p>
+                Accessible AI assistant provides instant support and guidance
+                for farmers.
+              </p>
+            </div>
           </div>
           <div className="f fe">
-          <div className="f_img4"></div>
+            <div className="co f_img4"></div>
+            <div className="con">
+              <h3>Weather Prediction</h3>
+              <p>
+                Accurate forecasts empower farmers for informed decision-making
+                and planning.
+              </p>
+            </div>
           </div>
         </div>
       </div>
       <div className="footer"></div>
+
       <div className="footer_content" alt="footer">
+        <div className="fconn">
+          <h2>Contact Us:-</h2>
+          <p>Phone No: +9374737829</p>
+          <a href="mailto:kaagreakdj@gmail.com">EMAIL US</a>
+        </div>
         <img src={footer} alt=""></img>
       </div>
     </div>
